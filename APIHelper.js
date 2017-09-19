45bd658ad3e43daf3a5fe6b25d10856b419fb87c
Ian Foose <ianfoose@me.com>
2017-09-18 21:09:39 -0400

    changed request function
    changed request function to return a promise and request

--- APIHelper.js
+++ APIHelper.js
@@ -1,15 +1,3 @@
-// request object
-var apiReq;
-
-/*
-* Cancel a request
-*
-* return void
-*/
-function cancelRequest() {
-	if(apiReq) { apiReq.abort(); }
-}
-
 /* make a request
 * 
 * params
@@ -17,84 +5,101 @@ function cancelRequest() {
 * url String
 * obj Optional overload params, such as headers and params 
 *
-* retruns a promise
+* retruns a request object
 */
 function request(method, url, obj) {
-	return new Promise(function(resolve,reject) {
-		if(url) {
-			if(!method) {
-				method = 'GET'; // defaults to get
-			} 
+	var apiReq;
 
-			apiReq = new XMLHttpRequest();
-			apiReq.open(method, url, true);
-
-			var params;
-
-			if(obj) {
-				if(obj.params) {
-					if(typeof obj.params !== 'object') { // not formdata 
-						apiReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-						params = obj.params;
-					} else {
-						var formData = new FormData();
-
-						for (var key in obj.params) {
-		    				formData.append(key, obj.params[key]);
-						}
-
-						params = formData;
-					}
-				}
-
-				if(obj.headers) {
-					for (var i = 0; i<obj.headers.length; i++) {
-						var header = obj.headers[i];
-						apiReq.setRequestHeader(header.key, header.value);
-					};
+	var requestObj = {
+		req:apiReq,
+		promise: new Promise(function(resolve, reject) {
+			if(url) {
+				if(!method) {
+					method = 'GET'; // defaults to get
 				} 
-			}
 
-			apiReq.onload = function(e) {
-				var resp = {
-					status:apiReq.status,
-					message:apiReq.statusText,
-					headers:apiReq.getAllResponseHeaders(),
-					text:apiReq.responseText,
-					json() {
-						return new Promise(function(resolve,reject) {
-							try {
-								result = JSON.parse(apiReq.responseText);
-								resolve(result);
-							} catch(error) {
-								reject(new Error('Invalid JSON')); 
+				apiReq = new XMLHttpRequest();
+				apiReq.open(method, url, true);
+
+				var params;
+
+				if(obj) {
+					if(obj.params) {
+						if(typeof obj.params !== 'object') { // not formdata 
+							apiReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+							params = obj.params;
+						} else {
+							var formData = new FormData();
+
+							for (var key in obj.params) {
+			    				formData.append(key, obj.params[key]);
 							}
-						});
-					},
-					validate() {
-						return new Promise(function(resolve,reject) {
-							if(apiReq.status >= 200 && apiReq.status <= 304 || apiReq.status == 409) {
-								resolve(resp);
-							}
-							return reject(new Error(apiReq.status+' Error'));
-						});
+
+							params = formData;
+						}
 					}
+
+					if(obj.headers) {
+						for (var i = 0; i<obj.headers.length; i++) {
+							var header = obj.headers[i];
+							apiReq.setRequestHeader(header.key, header.value);
+						};
+					} 
 				}
 
-				resolve(resp);
-			}
+				apiReq.onload = function(e) {
+					var resp = {
+						status:apiReq.status,
+						message:apiReq.statusText,
+						headers:apiReq.getAllResponseHeaders(),
+						text:apiReq.responseText,
+						json() {
+							return new Promise(function(resolve,reject) {
+								try {
+									result = JSON.parse(apiReq.responseText);
+									resolve(result);
+								} catch(error) {
+									reject(new Error('Invalid JSON')); 
+								}
+							});
+						},
+						validate() {
+							return new Promise(function(resolve,reject) {
+								if(apiReq.status >= 200 && apiReq.status <= 304 || apiReq.status == 409) {
+									resolve(resp);
+								}
 
-			apiReq.onerror = function() {
-				reject(new Error('Network Error'));
-			}
+								var msg = apiReq.status+' Error';
 
-			apiReq.onabort = function() {
-				apiReq = null;
+								if(apiReq.responseText) {
+									try {
+										result = JSON.parse(apiReq.responseText);
+										msg = result.result;
+									} catch(error) { }
+								}
+
+								return reject(new Error(msg));
+							});
+						}
+					}
+
+					resolve(resp);
+				}
+
+				apiReq.onerror = function() {
+					reject(new Error('Network Error'));
+				}
+
+				apiReq.onabort = function() {
+					apiReq = null;
+				}
+				
+				apiReq.send(params); 
+			} else {
+				reject(new Error('URL is empty, URL must not be empty'));
 			}
-			
-			apiReq.send(params); 
-		} else {
-			reject(new Error('URL is empty, URL must not be empty'));
-		}
-	});
+		})
+	}
+
+	return requestObj;
 }
